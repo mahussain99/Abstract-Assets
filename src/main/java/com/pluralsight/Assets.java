4f3e86a17a22e0c3727ec5972c31fec5ad800c37
Family <mh5212357@gmail.com>
2025-10-29 10:52:42 -0400

    I created all classes

---
+++ src/main/java/com/pluralsight/Assets.java
@@ -0,0 +1,43 @@
+public class Assets {
+
+    package com.pluralsight;
+
+        private String description;
+        private String dateAcquired;
+        private double originalCost;
+
+        public Assets(String description, String dateAcquired, double originalCost) {
+            this.description = description;
+            this.dateAcquired = dateAcquired;
+            this.originalCost = originalCost;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getDateAcquired() {
+            return dateAcquired;
+        }
+
+        public void setDateAcquired(String dateAcquired) {
+            this.dateAcquired = dateAcquired;
+        }
+
+        public double getOriginalCost() {
+            return originalCost;
+        }
+
+        public void setOriginalCost(double originalCost) {
+            this.originalCost = originalCost;
+        }
+
+        public double getValue() {
+            return originalCost;
+        }
+    }
+}
